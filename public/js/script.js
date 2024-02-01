a0dc5a526077e67eda359eb0380f643904386199
Rodrigo Melo <nalajejazz@gmail.com>
2024-01-31 23:32:15 -0300

    add frontend

--- public/js/script.js
+++ public/js/script.js
@@ -12,3 +12,8 @@
      document.getElementById('faleComAgente').className="border-warning border-bottom   border-2 rounded nav-link text-warning fs-5 active"
                   
  }
+else if(url_atual == '/quemSomos'){
+    
+    document.getElementById('quemSomos').className="border-warning border-bottom   border-2 rounded nav-link text-warning fs-5 active"
+                 
+}
